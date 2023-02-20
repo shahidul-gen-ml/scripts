6e0228ae69a9c603678e711d5f7b7982d13f4155
Shahidul Islam <islam.shahidul@genweb2.com>
2023-02-20 10:25:02 +0600

    video download v0.1

---
+++ download.py
@@ -0,0 +1,35 @@
+import csv
+import urllib.request
+import os
+import ssl
+from tqdm import tqdm
+import concurrent.futures
+
+DOWNLOAD_PATH = 'DOWNLOAD_VIDEO'
+
+if not os.path.exists(DOWNLOAD_PATH):
+    os.makedirs(DOWNLOAD_PATH)
+
+CSV_PATH = '/home/shahidul/Desktop/scripts/Training_Gloss_List_&_Details.xlsx - Gloss_Details.csv'
+
+HEADER = [
+    "idNumber", "annotationId", "attachmentId", "contentUrl", "fileName",
+    "sentence", "sentenceId", "signSentence", "userId", "meta", "word",
+    "start", "end"
+]
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+
+def download_file(url, filename):
+    urllib.request.urlretrieve(url, filename)
+
+
+file_list = [(row[HEADER[3]], f'{DOWNLOAD_PATH}/{row[HEADER[4]]}')
+             for row in csv.DictReader(open(CSV_PATH))]
+
+with concurrent.futures.ThreadPoolExecutor(max_workers=os.cpu_count() *
+                                           5) as executor:
+    list(
+        tqdm(executor.map(lambda x: download_file(*x), file_list),
+             total=len(file_list)))
